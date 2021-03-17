06b32506dfaa667a45e9f77727f409ac8d66f5d9
Jul <69659361+julsu2905@users.noreply.github.com>
2021-03-17 08:53:54 +0700

    Update categoryModel.js

--- api/models/categoryModel.js
+++ api/models/categoryModel.js
@@ -2,5 +2,11 @@ const mongoose = require('mongoose');
 
 const CategorySchema = new mongoose.Schema({
     name : String,
-    
+    products : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "Product"
+    }
 })
+
+const Category = mongoose.model("Category", productSchema, "categories");
+module.exports = Category;
